00b631e038878760df71505c156629447ae81873
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-08-02 23:23:05 +0000

    update cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,50 +66,51 @@ const Home = () => {
       </section>
 
       <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap w-full mb-20">
-      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-            Recommended Books For You
-        </h1>
-        <div class="h-1 w-20 bg-gray-500 rounded"></div>
-      </div>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
-
-      </p>
-    </div>
-    <div class="flex flex-wrap -m-4">
-        {
-            books.map((book) => (
-
-        
-      <div class="xl:w-1/4 md:w-1/2 p-4" key={book.id}>
-        <div class="bg-gray-100 p-1 rounded-lg">
-            <Link to={`/details/${book.id}`}>
-          <img class="h-64 rounded w-full object-cover object-center mb-6" src={
-                book.imageUrl
-          } alt="content"/>
-          </Link>
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">
-            {book.author}
-          </h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">
-            {book.title}
-          </h2>
-          <p class="leading-relaxed text-base">
-
-          </p>
-        </div>
-      </div>
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap w-full mb-20">
+            <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                Recommended Books For You
+              </h1>
+              <div class="h-1 w-20 bg-gray-500 rounded"></div>
+            </div>
+            <p class="lg:w-1/2 w-full leading-relaxed text-gray-500"></p>
+          </div>
+          <div class="flex flex-wrap -m-4">
+            {books.map((book) => (
+              <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <a class="block relative h-64 rounded overflow-hidden">
+                  <Link to={`/details/${book.id}`}>
+                    <img
+                      alt="ecommerce"
+                      class="object-cover object-center w-full h-full block"
+                      src={book.imageUrl}
+                    />
+                  </Link>
+                </a>
+                <div class="mt-4">
+                  <h3 class="text-gray-900 text-xs tracking-widest title-font mb-1">
+                    {book.author}
+                  </h3>
+                  <h2 class="text-gray-900 title-font text-lg font-medium">
+                    {book.title}
+                  </h2>
+                  <div className="flex justify-between">
+                    <p class="mt-1">{book.category}</p>
+                    <p class="mt-1">
+                      {book.copies > 0 ? (
+                        <h2>Available</h2>
+                      ) : (
+                        <h2>Not Available</h2>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
             ))}
-     
-    
-     
-    </div>
-  </div>
-</section>
-
-
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
